2f407226f2ab398247817036c92e6c54319dd0a7
Сытин Николай Борисович <30667669+kokdot@users.noreply.github.com>
2022-07-11 22:43:05 +0400

    hw_03_03

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -12,8 +12,7 @@ func Top10(s string) []string {
 	if s == "" {
 		return sliceResult
 	}
-	sliseForString := strings.Fields(s) // [one, two, three]
-	// fmt.Println(sliseForString)
+	sliseForString := strings.Fields(s)  // [one, two, three]
 	mapFrequency := make(map[string]int) // [one: 2, two: 3, three:2]
 	for _, word := range sliseForString {
 		mapFrequency[word]++
@@ -38,22 +37,16 @@ func Top10(s string) []string {
 		sliceNumbers = append(sliceNumbers, key)
 	}
 	sort.Ints(sliceNumbers)
-	// fmt.Println("sliceNumbers", sliceNumbers)
 	Numbers := 0
 	for i := 0; i < ConstNumbers; i++ {
-		// fmt.Println(i)
 		key := sliceNumbers[len(sliceNumbers)-1-i]
-		// fmt.Println("key", key)
-		// fmt.Println("mapNumbers" ,mapNumbers)
 		for _, v := range mapNumbers[key] {
 			sliceResult = append(sliceResult, v)
-			// fmt.Println("sliseResult", sliceResult)
 			Numbers++
 			if Numbers == ConstNumbers {
 				return sliceResult
 			}
 		}
-		// key++
 	}
 	return sliceResult
 }
