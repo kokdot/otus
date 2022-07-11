c551a62546f482644d6b064d626c2fbd19e1325c
Сытин Николай Борисович <30667669+kokdot@users.noreply.github.com>
2022-07-11 22:35:09 +0400

    hw_03_02

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,37 +1,38 @@
 package main
 
 import (
-	// "fmt"
-	"strings"
 	"sort"
+	"strings"
 )
-const ConstNumbers = 10
 
+const ConstNumbers = 10
 
 func Top10(s string) []string {
 	sliceResult := make([]string, 0)
 	if s == "" {
 		return sliceResult
 	}
-	sliseForString := strings.Fields(s) //[one, two, three]
+	sliseForString := strings.Fields(s) // [one, two, three]
 	// fmt.Println(sliseForString)
-	mapFrequency := make(map[string]int) //[one: 2, two: 3, three:2]
+	mapFrequency := make(map[string]int) // [one: 2, two: 3, three:2]
 	for _, word := range sliseForString {
-		if _, ok := mapFrequency[word]; ok {
-			mapFrequency[word]++
-		} else {
-			mapFrequency[word] = 1
-		}
+		mapFrequency[word]++
+		// if _, ok := mapFrequency[word]; ok {
+		// 	mapFrequency[word]++
+		// } else {
+		// 	mapFrequency[word] = 1
+		// }
 	}
 	mapNumbers := make(map[int][]string, 0) // [2:[one, two], 3:[three, for]]
 	for key, value := range mapFrequency {
-		if _, ok := mapNumbers[value]; ok {
-			mapNumbers[value] = append(mapNumbers[value], key)
-		} else {
-		mapNumbers[value] = []string{key}
-		}
+		mapNumbers[value] = append(mapNumbers[value], key)
+		// if _, ok := mapNumbers[value]; ok {
+		// 	mapNumbers[value] = append(mapNumbers[value], key)
+		// } else {
+		// 	mapNumbers[value] = []string{key}
+		// }
 	}
-	sliceNumbers := make([]int, 0) //[2, 3]
+	sliceNumbers := make([]int, 0) // [2, 3]
 	for key, value := range mapNumbers {
 		sort.Strings(value)
 		sliceNumbers = append(sliceNumbers, key)
@@ -41,7 +42,7 @@ func Top10(s string) []string {
 	Numbers := 0
 	for i := 0; i < ConstNumbers; i++ {
 		// fmt.Println(i)
-		key:= sliceNumbers[len(sliceNumbers) - 1 - i]
+		key := sliceNumbers[len(sliceNumbers)-1-i]
 		// fmt.Println("key", key)
 		// fmt.Println("mapNumbers" ,mapNumbers)
 		for _, v := range mapNumbers[key] {
