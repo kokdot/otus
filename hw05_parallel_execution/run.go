719f016f8a24b67baa6d959a9ee7cc54028b7291
Сытин Николай Борисович <30667669+kokdot@users.noreply.github.com>
2022-07-21 20:28:58 +0500

    hw05_01

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,144 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"fmt"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
 	// Place your code here.
+	tasksCount := len(tasks)
+	workersCount := n
+	maxNumber := tasksCount
+	maxErrorsCount := m
+	wg := sync.WaitGroup{}
+	var errSum int
+	var noErrSum int
+	var number int
+	var stopNumber int
+	checkTaskError := false
+	checkTaskErrorDone := false
+	checkMaxNumber := false
+	checkMaxNumberDone := false
+	checkTaskStop := false
+	checkTaskStopDone := false
+	checkTaskEnd := false
+	checkTaskEndDone := false
+	chTask := make(chan Task, tasksCount)
+	chStop := make(chan struct{}, n)
+	chStopGoRutine := make(chan struct{}, n)
+	chManageGoRutine := make(chan struct{}, n)
+	chGetMistake := make(chan struct{}, n)
+	wg.Add(workersCount)
+	for i := 0; i < workersCount; i++ {
+		go func(numberOfGoRutine int) {
+			defer wg.Done()
+			fmt.Println("Запущена горутина: ", numberOfGoRutine)
+			for {
+				select {
+				case task, ok := <-chTask:
+					if ok {
+						err := task()
+						if err != nil {
+							chGetMistake <- struct{}{}
+							fmt.Println("Выполнена задача с ошибкой", "numberOfGoRutine -- ", numberOfGoRutine)
+						} else {
+							chManageGoRutine <- struct{}{}
+							fmt.Println("Выполнена задача ", "numberOfGoRutine -- ", numberOfGoRutine)
+						}
+					}
+				case <-chStop:
+					fmt.Println("---------------Получено задане на остановку работы", " numberOfGoRutine -- ", numberOfGoRutine)
+					chStopGoRutine <- struct{}{}
+					return
+				}
+			}
+		}(i)
+	}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		fistCount := workersCount
+		if fistCount > maxNumber {
+			fistCount = maxNumber
+		}
+		for i := 0; i < fistCount; i++ {
+			number++
+			chTask <- tasks[i]
+		}
+		// fmt.Println("number = ", number, "; maxNumber = ", maxNumber)
+		for {
+			if ((number == maxNumber) || (errSum == maxErrorsCount)) && !checkTaskStopDone {
+				checkTaskStopDone = true
+				checkTaskStop = true
+			}
+			// fmt.Println("(((((((((((((((((((((((((((((((((((maxNumber ", maxNumber)
+
+			select {
+			case <-chGetMistake:
+				errSum++
+				if !checkTaskErrorDone {
+					checkTaskError = true
+					checkTaskErrorDone = true
+					if (workersCount + maxErrorsCount) < maxNumber {
+						maxNumber = workersCount + m
+					}
+				}
+				if !checkTaskStop {
+					number++
+					chTask <- tasks[number-1]
+				}
+				fmt.Println("  |   errSum = ", errSum, "; noErrSum = ",
+					noErrSum, "; maxNumber = ", maxNumber, "; number", number, "++++++++++")
+				fmt.Println("получ ошиб", "Общее кол ош: ", errSum, "Общее кол отпр заданий: ",
+					number, " checkTaskStop: ", checkTaskStop)
+
+			case <-chManageGoRutine:
+				noErrSum++
+				if !checkTaskStop {
+					number++
+					chTask <- tasks[number-1]
+				}
+				fmt.Println("Выполнено задание", "Общее кол заданий без ошибок: ",
+					noErrSum, "Общее кол отпр заданий: ", number)
+
+			case <-chStopGoRutine:
+				stopNumber++
+				fmt.Println("____________Остановлено горутин: ", stopNumber)
+			default:
+			}
+			if errSum+noErrSum == maxNumber && !checkMaxNumberDone {
+				fmt.Println("  |   errSum = ", errSum, "; noErrSum = ", noErrSum,
+					"; maxNumber = ", maxNumber, "; number", number, "++++++++++")
+				checkMaxNumber = true
+				checkMaxNumberDone = true
+			}
+			if checkMaxNumber {
+				checkMaxNumber = false
+				for i := 0; i < workersCount; i++ {
+					chStop <- struct{}{}
+				}
+				fmt.Println("  |   errSum = ", errSum, "; noErrSum = ", noErrSum,
+					"; maxNumber = ", maxNumber, "; number", number, "++++++++++")
+				fmt.Println("Выполнены все задания, флаг checkTaskStop установлен в true, отпрвлены задачи о завершении работы.")
+			}
+			if stopNumber == workersCount && !checkTaskEndDone {
+				checkTaskEndDone = true
+				checkTaskEnd = true
+			}
+
+			if checkTaskEnd {
+				return
+			}
+		}
+	}()
+	wg.Wait()
+	if checkTaskError {
+		return ErrErrorsLimitExceeded
+	}
 	return nil
 }
--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -16,6 +16,7 @@ func TestRun(t *testing.T) {
 	defer goleak.VerifyNone(t)
 
 	t.Run("if were errors in first M tasks, than finished not more N+M tasks", func(t *testing.T) {
+		// t.Skip()
 		tasksCount := 50
 		tasks := make([]Task, 0, tasksCount)
 
@@ -38,7 +39,44 @@ func TestRun(t *testing.T) {
 		require.LessOrEqual(t, runTasksCount, int32(workersCount+maxErrorsCount), "extra tasks were started")
 	})
 
+	t.Run("if m = 0, than finished after 1 error", func(t *testing.T) {
+		// t.Skip()
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+		var sumTime time.Duration
+
+		{
+			err := fmt.Errorf("error from task %d", 0)
+			tasks = append(tasks, func() error {
+				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+				atomic.AddInt32(&runTasksCount, 1)
+				return err
+			})
+		}
+
+		for i := 1; i < tasksCount; i++ {
+			taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
+			sumTime += taskSleep
+
+			tasks = append(tasks, func() error {
+				time.Sleep(taskSleep)
+				atomic.AddInt32(&runTasksCount, 1)
+				return nil
+			})
+		}
+
+		workersCount := 10
+		maxErrorsCount := 0
+		err := Run(tasks, workersCount, maxErrorsCount)
+
+		require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
+		require.LessOrEqual(t, runTasksCount, int32(workersCount+maxErrorsCount), "extra tasks were started")
+	})
+
 	t.Run("tasks without errors", func(t *testing.T) {
+		// t.Skip()
 		tasksCount := 50
 		tasks := make([]Task, 0, tasksCount)
 
@@ -57,13 +95,14 @@ func TestRun(t *testing.T) {
 		}
 
 		workersCount := 5
+		// workersCount := 60
 		maxErrorsCount := 1
 
 		start := time.Now()
 		err := Run(tasks, workersCount, maxErrorsCount)
 		elapsedTime := time.Since(start)
 		require.NoError(t, err)
-
+		fmt.Println("runTasksCount :  --", runTasksCount)
 		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
 	})
