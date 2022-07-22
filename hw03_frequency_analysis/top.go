671bfc49ca8ba472fb8c265287773db8abe0c1a7
Сытин Николай Борисович <30667669+kokdot@users.noreply.github.com>
2022-07-22 09:44:09 +0500

    hw03_06

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -8,7 +8,7 @@ import (
 const ConstNumbers = 10
 
 func Top10(s string) []string {
-	sliceResult := make([]string, 0)
+	var sliceResult []string
 	if s == "" {
 		return sliceResult
 	}
@@ -16,37 +16,17 @@ func Top10(s string) []string {
 	mapFrequency := make(map[string]int) // [one: 2, two: 3, three:2]
 	for _, word := range sliseForString {
 		mapFrequency[word]++
-		// if _, ok := mapFrequency[word]; ok {
-		// 	mapFrequency[word]++
-		// } else {
-		// 	mapFrequency[word] = 1
-		// }
 	}
-	mapNumbers := make(map[int][]string) // [2:[one, two], 3:[three, for]]
-	for key, value := range mapFrequency {
-		mapNumbers[value] = append(mapNumbers[value], key)
-		// if _, ok := mapNumbers[value]; ok {
-		// 	mapNumbers[value] = append(mapNumbers[value], key)
-		// } else {
-		// 	mapNumbers[value] = []string{key}
-		// }
+
+	for key := range mapFrequency {
+		sliceResult = append(sliceResult, key)
 	}
-	sliceNumbers := make([]int, 0) // [2, 3]
-	for key, value := range mapNumbers {
-		sort.Strings(value)
-		sliceNumbers = append(sliceNumbers, key)
-	}
-	sort.Ints(sliceNumbers)
-	Numbers := 0
-	for i := 0; i < ConstNumbers; i++ {
-		key := sliceNumbers[len(sliceNumbers)-1-i]
-		for _, v := range mapNumbers[key] {
-			sliceResult = append(sliceResult, v)
-			Numbers++
-			if Numbers == ConstNumbers {
-				return sliceResult
-			}
+	sort.Slice(sliceResult, func(i, j int) bool {
+		if mapFrequency[sliceResult[i]] == mapFrequency[sliceResult[j]] {
+			return sliceResult[i] < sliceResult[j]
 		}
-	}
-	return sliceResult
+		return mapFrequency[sliceResult[i]] > mapFrequency[sliceResult[j]]
+	})
+
+	return sliceResult[:ConstNumbers]
 }
