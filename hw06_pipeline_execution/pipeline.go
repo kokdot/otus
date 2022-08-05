1e6581344f8e96f2a59d85a6d43e06aac61198bc
kokdot <kok.74@mail.ru>
2022-08-05 21:45:22 +0500

    hw06_after_lesson

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,7 +1,8 @@
 package hw06pipelineexecution
-import(
-	"fmt"
-)
+
+// import "github.com/sqs/goreturns/returns"
+
+// "fmt"
 
 type (
 	In  = <-chan interface{}
@@ -11,47 +12,33 @@ type (
 
 type Stage func(in In) (out Out)
 
+func wrap(in In, done In) Out {
+	out := make(Bi)
+	go func() () {
+		defer close(out)
+		for {
+			select {
+			case <- done:
+				return
+			case v, ok := <- in:
+				if !ok {
+					return
+				}
+				out <- v
+				// return
+			}
+		}
+	}()
+	return out
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	fmt.Println("stages")
-	fmt.Println(len(stages))
-	// if in == nil {
-	// 	return nil
-	// }
-	select {
-	case <- done:
-		terminate := make(Bi)
-		defer close(terminate)
-		return terminate
-	default:
-		// return in 
-	}
+	// out := make(chan Out)
+	out := in
+	for i := 0; i< len(stages); i++ {
+		// out = stages[i](out)
+		out = stages[i](wrap(out, done))
 
-	if len(stages) == 0 {
-		return in
 	}
-
-	// if len(stages) == 0 {
-	// 	return in 
-	// }
-	// out := ExecutePipeline(in, done, stages...)
-	// stage := stages[0]
-	// stages = stages[1:]
-	
-	select {
-	case <- done:
-		fmt.Println("PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP")
-		return nil
-	default:
-	// case <- in:
-	}
-	
-	// go func(done In) () {
-		// 	done
-		// }(done)
-		
-	stage := stages[0]
-	stages = stages[1:]
-	return ExecutePipeline(stage(in), done, stages...)
-	// return ExecutePipeline(stage(in), done, stages...)
-
+	return out
 }
