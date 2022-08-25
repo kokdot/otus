f068d5840fd69954bcd18d235563e5e460cca3ce
kokdot <kok.74@mail.ru>
2022-08-25 11:10:52 +0500

    first

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,48 @@ package main
 
 import (
 	"errors"
+	"os"
+	"time"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
-var (
-	ErrUnsupportedFile       = errors.New("unsupported file")
-	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
-)
+// ErrUnsupportedFile = errors.New("unsupported file").
+
+var ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
 	// Place your code here.
+	file, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	bSrc, _ := os.ReadFile(fromPath)
+	if int(offset) > len(bSrc) {
+		return ErrOffsetExceedsFileSize
+	}
+	count := 100
+
+	// create and start new bar
+	bar := pb.StartNew(count)
+
+	for i := 0; i < count; i++ {
+		bar.Increment()
+		time.Sleep(time.Millisecond)
+	}
+
+	// finish bar
+	bar.Finish()
+	if offset > 0 {
+		bSrc = bSrc[offset:]
+	}
+	if limit > 0 {
+		if len(bSrc) < int(limit) {
+			limit = int64(len(bSrc))
+		}
+		bSrc = bSrc[:limit]
+	}
+	file.Write(bSrc)
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,81 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	srcDir    = "./testdata/"
+	dstDir    = "./testdata/testdir/"
+	fileInput = "input.txt"
+	fileTest1 = "out_offset0_limit0.txt"
+	fileTest2 = "out_offset0_limit10.txt"
+	fileTest3 = "out_offset0_limit1000.txt"
+	fileTest4 = "out_offset0_limit10000.txt"
+	fileTest5 = "out_offset100_limit1000.txt"
+	fileTest6 = "out_offset6000_limit1000.txt"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	_ = os.Mkdir(dstDir, 0o755)
+	defer os.RemoveAll(dstDir)
+	fromPath := srcDir + fileInput
+
+	t.Run("case offset0_limit0", func(t *testing.T) {
+		t.Skip()
+		toPath := dstDir + fileTest1
+		toTest := srcDir + fileTest1
+		_ = Copy(fromPath, toPath, 0, 0)
+		result, _ := os.ReadFile(toPath)
+		test, _ := os.ReadFile(toTest)
+		require.Equal(t, result, test)
+	})
+
+	t.Run("case out_offset0_limit10", func(t *testing.T) {
+		toPath := dstDir + fileTest2
+		toTest := srcDir + fileTest2
+		_ = Copy(fromPath, toPath, 0, 10)
+		result, _ := os.ReadFile(toPath)
+		test, _ := os.ReadFile(toTest)
+		require.Equal(t, result, test)
+	})
+
+	t.Run("case out_offset0_limit1000", func(t *testing.T) {
+		toPath := dstDir + fileTest3
+		toTest := srcDir + fileTest3
+		_ = Copy(fromPath, toPath, 0, 1000)
+		result, _ := os.ReadFile(toPath)
+		test, _ := os.ReadFile(toTest)
+		require.Equal(t, result, test)
+	})
+
+	t.Run("case out_offset0_limit100000", func(t *testing.T) {
+		toPath := dstDir + fileTest4
+		toTest := srcDir + fileTest4
+		_ = Copy(fromPath, toPath, 0, 10000)
+		result, _ := os.ReadFile(toPath)
+		test, _ := os.ReadFile(toTest)
+		require.Equal(t, result, test)
+	})
+
+	t.Run("case out_offset100_limit1000", func(t *testing.T) {
+		toPath := dstDir + fileTest5
+		toTest := srcDir + fileTest5
+		_ = Copy(fromPath, toPath, 100, 1000)
+		result, _ := os.ReadFile(toPath)
+		test, _ := os.ReadFile(toTest)
+		require.Equal(t, result, test)
+	})
+
+	t.Run("case out_offset6000_limit1000", func(t *testing.T) {
+		toPath := dstDir + fileTest6
+		toTest := srcDir + fileTest6
+		_ = Copy(fromPath, toPath, 6000, 1000)
+		result, _ := os.ReadFile(toPath)
+		test, _ := os.ReadFile(toTest)
+		require.Equal(t, result, test)
+	})
 }
