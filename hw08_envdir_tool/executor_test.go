5ce39d92a4d01d0606095f513e7a7bab955ce33b
kokdot <kok.74@mail.ru>
2022-09-14 14:56:13 +0500

    second_4

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -11,8 +11,10 @@ func TestRunCmd(t *testing.T) {
 		log.Fatal(err)
 	}
 	// _ = RunCmd([]string{"ls"}, env)
-	_ = RunCmd([]string{"/mnt/c/Users/user/Documents/go/otus/otus/hw08_envdir_tool//testdata/env",
-		"/bin/bash", "/mnt/c/Users/user/Documents/go/otus/otus/hw08_envdir_tool//testdata/echo.sh", "arg1=1", "arg2=2"}, env)
+	_ = RunCmd([]string{
+		"/mnt/c/Users/user/Documents/go/otus/otus/hw08_envdir_tool//testdata/env",
+		"/bin/bash", "/mnt/c/Users/user/Documents/go/otus/otus/hw08_envdir_tool//testdata/echo.sh", "arg1=1", "arg2=2",
+	}, env)
 	// _ = RunCmd([]string{"./testdata/echo.sh", "arg1=1", "arg2=2"}, env)
 	// return result
 }
