4b5d634a54706657edb35f57e824fd7cef17f8c3
kokdot <kok.74@mail.ru>
2022-09-16 20:21:55 +0500

    second_7

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,22 +1,22 @@
 package main
 
-import (
-	"log"
-	"testing"
-)
+// import (
+// 	"log"
+// 	"testing"
+// )
 
-func TestRunCmd(t *testing.T) {
-	// t.Skip()
-	env, err := ReadDir("testdata/env/")
-	if err != nil {
-		log.Fatal(err)
-	}
-	_ = RunCmd([]string{
-		"./testdata/env",
-		"/bin/bash", 
-		"./testdata/echo.sh",
-		"arg1=1",
-		"arg2=2",
-	},
-		env)
-}
+// func TestRunCmd(t *testing.T) {
+// 	// t.Skip()
+// 	env, err := ReadDir("testdata/env/")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	_ = RunCmd([]string{
+// 		"./testdata/env",
+// 		"/bin/bash", 
+// 		"./testdata/echo.sh",
+// 		"arg1=1",
+// 		"arg2=2",
+// 	},
+// 		env)
+// }
